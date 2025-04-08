4b1fb5dd0ca43447c7339ee870cc06c961d8553f
John Owens <john.owens@opencastsoftware.com>
2025-04-08 14:48:50 +0100

    feat: added function to print employee details

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rust_sandbox::structs::employee::Employee;
+
 fn main() {
     println!("Hello, world!");
+
+    print_employee("Paul".to_string(), "Smith".to_string(), "Web Developer".to_string(), "Software Delivery".to_string());
+    print_employee("Laura".to_string(), "Smith".to_string(), "Business Analyst".to_string(), "Product Delivery".to_string());
+    print_employee("David".to_string(), "Smith".to_string(), "Product Manager".to_string(), "Product Delivery".to_string());
+    print_employee("Jane".to_string(), "Smith".to_string(), "QA Tester".to_string(), "Software Delivery".to_string());
+}
+
+fn print_employee(first_name: String, last_name: String, role: String, department: String) {
+    let employee = Employee {
+        first_name,
+        last_name,
+        role,
+        department
+    };
+
+    println!("{}", employee.to_string());
 }
